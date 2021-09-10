46520acdb456a4d960659db47c63c8fb8a48f416
antoni0o <antonio.dias8997@gmail.com>
2021-09-09 23:38:51 -0300

    class2.1

--- Backend/src/uploadHandler.js
+++ Backend/src/uploadHandler.js
@@ -4,14 +4,37 @@ import { pipeline } from 'stream/promises'
 import { logger } from './logger'
 
 export default class UploadHandler {
-  constructor({ io, socketId, downloadsFolder }) {
+  constructor({ io, socketId, downloadsFolder, messageTimeDelay = 200 }) {
     this.io = io
     this.socketId = socketId 
     this.downloadsFolder = downloadsFolder
+    this.ON_UPLOAD_EVENT = 'file-upload'
+    this.messageTimeDelay = messageTimeDelay
   }
 
-  handleFileBytes() {
+  canExecute(lastExecution) {
+    return (Date.now() - lastExecution) > this.messageTimeDelay
+  }
 
+  handleFileBytes(filename) {
+    this.lastMessageSent = Date.now()
+
+    async function* handleData(source) {
+      let processedAlready = 0
+  
+      for await(const chunk of source) {
+        yield chunk
+        processedAlready += chunk.length
+        
+        if(!this.canExecute(this.lastMessageSent)) {
+          continue
+        }
+
+        this.io.to(this.socketId).emit(this.ON_UPLOAD_EVENT, { processedAlready, filename })
+        logger.info(`File [${filename}] got ${processedAlready} bytes to ${this.socketId}`)
+      }
+    }
+    return handleData.bind(this)
   }
 
   async onFile(fieldname, file, filename) {
--- Backend/test/unit/uploadHandler.test.js
+++ Backend/test/unit/uploadHandler.test.js
@@ -1,7 +1,15 @@
 import Routes from '../../src/routes.js';
+import { logger } from '../../src/logger.js';
 import UploadHandler from "../../src/uploadHandler.js";
 import TestUtil from '../utils/testUtil.js';
-import { jest } from "@jest/globals";
+import {
+  describe,
+  test,
+  expect,
+  beforeEach,
+  jest
+} from "@jest/globals";
+import { pipeline } from 'stream/promises';
 import fs from 'fs'
 import { resolve } from 'path'
 
@@ -10,6 +18,11 @@ describe('#UploadHandler test suite', () => {
     to: (id) => ioObj,
     emit: (event, message) => {},
   };
+
+  beforeEach(() => {
+    jest.spyOn(logger, 'info')
+        .mockImplementation()
+  })
 
   describe('#registerEvents', () => {
     test('should call onFile and onFinish functions on Busboy instance', () => {
@@ -71,6 +84,79 @@ describe('#UploadHandler test suite', () => {
 
       const expectedFilename = resolve(handler.downloadsFolder, params.filename)
       expect(fs.createWriteStream).toHaveBeenCalledWith(expectedFilename)
+    })
+  })
+
+  describe('#handleFileBytes', () => {
+    test('should call emit function and it is a transform stream', async () => {
+      jest.spyOn(ioObj, ioObj.to.name)
+      jest.spyOn(ioObj, ioObj.emit.name)
+
+      const handler = new UploadHandler({
+        io: ioObj,
+        socketId: '01'
+      })
+
+      jest.spyOn(handler, handler.canExecute.name)
+          .mockReturnValue(true)
+
+      const messages = ['hello'],
+            source = TestUtil.generateReadableStream(messages),
+            onWrite = jest.fn(),
+            target = TestUtil.genereateWritableStream(onWrite)
+
+      await pipeline(
+        source,
+        handler.handleFileBytes("filename.txt"),
+        target
+      )
+
+      expect(ioObj.to).toHaveBeenCalledTimes(messages.length)
+      expect(ioObj.emit).toHaveBeenCalledTimes(messages.length)
+
+      expect(onWrite).toBeCalledTimes(messages.length)
+      expect(onWrite.mock.calls.join()).toEqual(messages.join())
+    })
+  })
+
+  describe('#canExecute', () => {
+
+    test('should return true when time is later than specified delay', () => {
+      
+      const timerDelay = 1000,
+            uploadHandler = new UploadHandler({
+              io: {},
+              socketId: '',
+              messageTimeDelay: timerDelay
+            })
+            
+      const now = TestUtil.getTimeFromDate('2021-07-01 00:00:02')
+      TestUtil.mockDateNow([now])
+            
+      const lastExecution = TestUtil.getTimeFromDate('2021-07-01 00:00:00')
+            
+      const result = uploadHandler.canExecute(lastExecution)
+      
+
+      expect(result).toBeTruthy()
+    })
+    test('should return false when time isnt later than specified delay', () => {
+      const timerDelay = 3000,
+            uploadHandler = new UploadHandler({
+              io: {},
+              socketId: '',
+              messageTimeDelay: timerDelay
+            })
+            
+      const now = TestUtil.getTimeFromDate('2021-07-01 00:00:02')
+      TestUtil.mockDateNow([now])
+      
+      const lastExecution = TestUtil.getTimeFromDate('2021-07-01 00:00:01')
+            
+      const result = uploadHandler.canExecute(lastExecution)
+      
+
+      expect(result).toBeFalsy()
     })
   })
 })
